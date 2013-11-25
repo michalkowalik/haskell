e24ff0ab98af9e96a77372e2961122d699190b56
michal.kowalik <mkowalik@outlook.com>
2013-11-25 13:36:32 +0100

    display board in a reasonable format

--- bug.hs
+++ bug.hs
@@ -18,10 +18,22 @@ bc = [(Bug x y) | x <- [0..(size - 1)], y <- [0..(size - 1)]]
 board :: [Bug] -> [((Integer, Integer), Int)]
 board bc = [((x,y), length $ filter (\a -> a == Bug x y) bc) | x <- [0..(size - 1)], y <- [0..(size - 1)]]
 
+-- a simple view of a single board line:
+--lineView :: Integer -> [((Integer, Integer), Int)] -> [Char]
+--lineView line b = (unwords [show $ snd x | x <- b, (fst (fst x)) == line]) ++ "\n"
+
+-- a simple view of a complete board:
+--boardView :: [((Integer, Integer), Int)] -> [Char]
+--boardView b = unwords [lineView x b | x <- [0..(size - 1)]]
+
+boardView :: [((Integer, Integer), Int)] -> [Char]
+boardView b = unwords [lineView (filter (\a -> (fst (fst a)) == x) b) | x <- [0..(size - 1)]]
+               where lineView bb = (unwords [show $ snd x | x <- bb]) ++ "\n"
+
 -- move bug:
 move :: (Integer, Integer) -> Bug -> Bug
-move (dx,dy) (Bug x y) = if (x + dx) < size && (y + dy) < size && (x+dx) >= 0 && (y+dy) >= 0 then
-                           Bug (x+dx) (y+dy)
+move (dx,dy) (Bug x y) = if (x + dx) < size && (y + dy) < size && (x + dx) >= 0 && (y + dy) >= 0 then
+                           Bug (x + dx) (y + dy)
                          else
                            Bug x y
 
@@ -37,13 +49,15 @@ deltas = do
 main :: IO ()
 main = do
   putStrLn "bugs on the cheesboard, initial state:"
-  print $ board bc
+  putStrLn $ boardView $ board bc
   d <- deltas
   
   let b = board $ [move (d!!x) (bc!!x) | x <- [0..224]]
   putStrLn "Modified table:"
-  print b
+  --print b
+  putStrLn $ boardView b
 
   putStrLn ("sum of bugs on the board: " ++  show (sum $ map snd b))
   putStrLn "Max value on the board: "
   print $ [x | x <- b, (snd x) == (maximum $ map snd b)]
+--  putStrLn $ boardView b
