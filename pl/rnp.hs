0a97b8373712c3536d8f2bc4411abc07e23605ec
michal.kowalik <mkowalik@outlook.com>
2015-07-20 11:34:44 +0200

    modifying rpn calculator

--- pl/rnp.hs
+++ pl/rnp.hs
@@ -10,3 +10,26 @@ solveRPN = head . foldl foldingFunction [] . words
           foldingFunction (x:xs) "ln" = log x:xs
           foldingFunction xs "sum" = [sum xs]
           foldingFunction xs numberString = read numberString:xs  
+
+
+solveRPN' :: String -> [Float]
+solveRPN' = foldl foldingFunction [] . words  
+  where   foldingFunction (x:y:ys) "*" = (x * y):ys  
+          foldingFunction (x:y:ys) "+" = (x + y):ys  
+          foldingFunction (x:y:ys) "-" = (y - x):ys
+          foldingFunction (x:y:ys) "/" = (y / x):ys
+          foldingFunction (x:y:ys) "^" = (y ** x):ys
+          foldingFunction (x:xs) "ln" = log x:xs
+          foldingFunction xs "sum" = [sum xs]
+          foldingFunction xs numberString = read numberString:xs  
+
+
+foldingFunction :: [Float] -> String -> [Float]
+foldingFunction (x:y:ys) "*" = (x * y):ys  
+foldingFunction (x:y:ys) "+" = (x + y):ys  
+foldingFunction (x:y:ys) "-" = (y - x):ys
+foldingFunction (x:y:ys) "/" = (y / x):ys
+foldingFunction (x:y:ys) "^" = (y ** x):ys
+foldingFunction (x:xs) "ln" = log x:xs
+foldingFunction xs "sum" = [sum xs]
+foldingFunction xs numberString = read numberString:xs  
